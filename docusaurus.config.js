cdb24d03fb6f76fcb272c0042ccb178fb225140c
Vladimir Volek <volek.vladimir@gmail.com>
2023-08-29 18:06:02 +0200

    fix: broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ module.exports = {
   tagline: "Welcome to the Blockfrost.io universe",
   url: "https://blockfrost.dev/",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "blockfrost",
